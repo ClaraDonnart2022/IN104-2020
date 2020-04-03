f22a0937e33347e52c362a6f9ef55c23b2f1e4dd
Jean-Didier Garaud <48656672+JDGaraudEnsta@users.noreply.github.com>
2020-04-03 11:23:25 +0200

    ajout card2.py

---
+++ TD2/card2.py
@@ -0,0 +1,42 @@
+"""
+Cartes 
+"""
+
+
+COEUR, CARREAU, PIQUE, TREFLE = (0, 1, 2, 3)
+#valeur : 7, 8, 9, 10, 11, 12, 13, 14 
+
+class Card:
+    def __init__(self, couleur, valeur):
+        self.couleur = couleur
+        self.valeur = valeur
+        
+    
+
+
+card1 = Card()
+card1.couleur = COEUR
+card1.valeur = 10
+
+card2 = Card()
+card2.valeur = 9
+card2.couleur = COEUR
+
+#
+print("Le joueur 1 joue la carte", card1) 
+print("Le joueur 2 joue la carte", card2)
+print("qui a la meilleure carte ?")
+
+## maintenant on compare des cartes
+# atout ? 
+
+if card1.couleur == card2.couleur:
+    if card1.valeur > card2.valeur :
+        print("C'est J1 qui gagne avec", card1)
+    else:
+        print("C'est J2 qui gagne avec", card2)
+else:
+    print("FIXME")
+        
+
+print("Et va vaut ", card1.points + card2.points)
