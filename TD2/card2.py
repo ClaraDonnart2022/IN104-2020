d1dd1e10bfa60d992e604527111cf60b854006a4
Jean-Didier Garaud <48656672+JDGaraudEnsta@users.noreply.github.com>
2020-04-03 11:38:11 +0200

    Card2 , avec les TODO pour la semaine prochaine

--- TD2/card2.py
+++ TD2/card2.py
@@ -1,5 +1,16 @@
 """
 Cartes 
+
+
+TODO: 
+
+  - coder la fonction str
+  - le init qui prend 0 ou 2 parametres
+  - coder la fonction(? ou attribut ?) points
+  - corriger le bug : qui est que le 10 est plus fort que Dame Roi Valet (7 8 9 V D R 10 As) 
+
+  - bonus : ajouter les atouts
+  - bonus : ajouter un setter qui empeche couleur = "VERTE"
 """
 
 
@@ -12,16 +23,24 @@ class Card:
         self.valeur = None
         self.points = 0
 
-        
     # def __init__(self, couleur, valeur):
     #     self.couleur = couleur
     #     self.valeur = valeur
+
+    def __str__(self):
+        s = "coucou"   #FIXME
+        return s
+        
     
 
+# card1 = Card(10, COEUR)
 
 card1 = Card()
 card1.couleur = COEUR
 card1.valeur = 10
+
+card1.couleur = "VERTE"
+
 
 card2 = Card()
 card2.valeur = 9
