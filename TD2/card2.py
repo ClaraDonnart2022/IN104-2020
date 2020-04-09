39a2428adba467575421399ee12e501ba3f1d593
Quach Christine <christine.quach.2022@ensta-paris.fr>
2020-04-09 18:25:24 +0200

    version 1

--- TD2/card2.py
+++ TD2/card2.py
@@ -20,49 +20,47 @@ COEUR, CARREAU, PIQUE, TREFLE = (0, 1, 2, 3)
 #valeur : 7, 8, 9, 10, 11, 12, 13, 14 
 
 class Card:
-    def __init__(self):
-        self.couleur = None
-        self.valeur = None
-        self.points = 0
+    def __init__(self, valeur, couleur, points):
+        self.couleur = couleur
+        self.valeur = valeur
+        self.points = points
 
     # def __init__(self, couleur, valeur):
     #     self.couleur = couleur
     #     self.valeur = valeur
 
     def __str__(self):
-        s = "coucou"   #FIXME
-        return s
-        
+        return str(self.valeur) +" de "+ str(self.couleur)
+
+
+
+
     
 
 # card1 = Card(10, COEUR)
 
-card1 = Card()
-card1.couleur = COEUR
-card1.valeur = 10
+card1 = Card(10,COEUR,10)
+
 
 # card1.couleur = "VERTE"
 
 
-card2 = Card()
-card2.valeur = 9
-card2.couleur = COEUR
+card2 = Card(9,COEUR,0)
 
 #
-print("Le joueur 1 joue la carte", card1) 
-print("Le joueur 2 joue la carte", card2)
+print("Le joueur 1 joue la carte ", card1) 
+print("Le joueur 2 joue la carte ", card2) 
 print("qui a la meilleure carte ?")
 
 ## maintenant on compare des cartes
 # atout ? 
+A=[0,0,0,0,0,0,0,7,8,9,13,10,11,12,14]
 
 if card1.couleur == card2.couleur:
-    if card1.valeur > card2.valeur :
-        print("C'est J1 qui gagne avec", card1)
+    if A[card1.valeur] > A[card2.valeur] :
+        print("C'est J1 qui gagne avec ", card1) 
     else:
-        print("C'est J2 qui gagne avec", card2)
-else:
-    print("FIXME")
+        print("C'est J2 qui gagne avec", card2) 
         
 
-print("Et va vaut ", card1.points + card2.points)
+print("Et va vaut ", card1.points + card2.points," points.")
