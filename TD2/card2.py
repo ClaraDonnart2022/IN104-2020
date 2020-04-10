97575ac4f9e99383f527ab7dbbd8abb3cadb9b80
Yon <guillaume.yon@ensta-paris.fr>
2020-04-10 11:38:50 +0200

    Version de Atoli et Guillaume

--- TD2/card2.py
+++ TD2/card2.py
@@ -1,68 +1,70 @@
 """
 Module card2.
-
 Definit une classe Card et un petit cas-test qui demontre son utilite.
-
-
 TODO: 
-
   - coder la fonction str
   - le init qui prend 0 ou 2 parametres (indication : chercher sur StackOverflow "python optional arguments in initializer of python class")
   - coder points ; d'ailleurs, est-ce une fonction ou un attribut ? 
   - corriger le bug : qui est que le 10 est plus fort que Dame Roi Valet (7 8 9 V D R 10 As) 
-
   - bonus : ajouter les atouts
   - bonus : ajouter un setter qui empeche couleur = "VERTE"
 """
+from enum import Enum
 
 
-COEUR, CARREAU, PIQUE, TREFLE = (0, 1, 2, 3)
-#valeur : 7, 8, 9, 10, 11, 12, 13, 14 
+Color = Enum("Color", "coeur carreau pique trefle")
+	
+Valeur = Enum("Valeur", "SEPT HUIT NEUF VALET DAME ROI DIX AS")
 
 class Card:
-    def __init__(self):
-        self.couleur = None
-        self.valeur = None
-        self.points = 0
+	def __init__(self, valeur = None, couleur = None):
+		self.couleur = couleur
+		self.valeur = valeur
 
-    # def __init__(self, couleur, valeur):
-    #     self.couleur = couleur
-    #     self.valeur = valeur
 
-    def __str__(self):
-        s = "coucou"   #FIXME
-        return s
+	def __str__(self):
+		p = str(Color(self.couleur).name)
+		q = str(Valeur(self.valeur).name)
+		return (q + " de " + p)
         
-    
-
-# card1 = Card(10, COEUR)
-
-card1 = Card()
-card1.couleur = COEUR
-card1.valeur = 10
-
-# card1.couleur = "VERTE"
-
-
-card2 = Card()
-card2.valeur = 9
-card2.couleur = COEUR
-
-#
+	def points_carte (self):
+		if (self.valeur == Valeur.SEPT) or (self.valeur == Valeur.HUIT) or (self.valeur == Valeur.NEUF):
+			self.points = 0
+		if self.valeur == Valeur.DIX :
+			self.points = 10
+		if self.valeur == Valeur.VALET:
+			self.points = 2
+		if self.valeur == Valeur.DAME :
+			self.points = 3
+		if self.valeur == Valeur.ROI :
+			self.points = 4
+		if self.valeur ==  Valeur.AS :
+			self.points = 11
+	def strength(self):
+		return self.valeur.value
+card1 = Card(Valeur.ROI, Color.PIQUE)
+card2 = Card(Valeur.DIX, Color.PIQUE)
 print("Le joueur 1 joue la carte", card1) 
 print("Le joueur 2 joue la carte", card2)
 print("qui a la meilleure carte ?")
 
 ## maintenant on compare des cartes
 # atout ? 
+p1 = card1.points
+p2 = card2.points
 
-if card1.couleur == card2.couleur:
-    if card1.valeur > card2.valeur :
-        print("C'est J1 qui gagne avec", card1)
-    else:
-        print("C'est J2 qui gagne avec", card2)
+if p1 == p2:
+    print("Égalité") # Ce cas n'est pas censé se produire dans le cadre de la belotte
+elif p1 > p2:
+    print("C'est J1 qui gagne avec", card1)
 else:
-    print("FIXME")
+    print("C'est J2 qui gagne avec",card2)
+			
+		
+else:
+	print("FIXME")
         
 
-print("Et va vaut ", card1.points + card2.points)
+print("Et ça vaut ", card1.points + card2.points)
+
+ 
